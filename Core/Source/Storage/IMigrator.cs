9ded0fb9518ec4f13c5594731807921e4296f170
Roslik Ivan <iroslik.dev@dent-it.ru>
2025-05-05 19:10:38 +0300

    storage updater started

---
+++ Core/Source/Storage/IMigrator.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskTrain.Core
+{
+    public interface IMigration 
+    {
+        string GetQueryText();
+    }
+
+    public interface IMigrator
+    {
+        IEnumerable<IMigration> GetMigrations();
+        uint GetLastVersion();
+        uint GetCurrentVersion();
+        void StepForward();
+        void StepBackward();
+    }
+}
---
+++ Core/Source/Storage/StorageUpdaterBase.cs
@@ -0,0 +1,36 @@
+﻿namespace TaskTrain.Core;
+
+public abstract class StorageUpdaterBase
+{
+    public event Action<string> OnUpdatedInstalledSucced;
+    public event Action<string> OnUpdatedInstalledFailed;
+
+    protected readonly IMigrator _migrator;
+    protected readonly string _connectionString;
+
+    protected StorageUpdaterBase(string connectionString
+        , IMigrator migrator)
+    {
+        _migrator = migrator;
+        _connectionString = connectionString;
+    }
+
+    protected abstract bool CheckConnection();
+
+    /* [TODO]
+     * - IMigration could be soted in disk or might be accessible remotly(fancy staff)
+     * - implement logic to for this method
+     */
+    public void UpdateStorage(uint targetVersion)
+    {
+        const string failedMessagePrefix = "Failed to update storage: ";
+        if (!CheckConnection())
+        {
+            OnUpdatedInstalledFailed?.Invoke($"{failedMessagePrefix} couldn't connect to storage!" +
+                $" Check connection string and try again");
+            return;
+        }
+
+        OnUpdatedInstalledSucced?.Invoke($"Succesufly update storage to version: {targetVersion}");
+    }
+}
