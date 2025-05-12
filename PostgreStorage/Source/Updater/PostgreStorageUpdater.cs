dd26efed78b069ef006dc63492ae6764acb512d3
Roslik Ivan <iroslik.dev@dent-it.ru>
2025-05-12 18:01:26 +0300

    - starting implement storage updater for postgreSQL - gitignore add directory '_publish'

---
+++ PostgreStorage/Source/Updater/PostgreStorageUpdater.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+
+namespace TaskTrain.Core.Postgres;
+
+public sealed class PostgreStorageUpdater : StorageUpdaterBase
+{
+    private readonly NpgsqlDataSource _dataSource;
+
+    public PostgreStorageUpdater(string connectionString) : base(connectionString)
+    {
+        _dataSource = NpgsqlDataSource.Create(connectionString);
+    }
+
+    protected override void ExecuteMigarionQuery(string queryText)
+    {
+        using (var cmd = _dataSource.CreateCommand("select 1;")) 
+        {
+
+        }
+    }
+
+    protected override uint GetCurrentVersion()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override uint GetLastVersion()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override IMigration[] GetMigrations()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override bool IsAvailable()
+    {
+        throw new NotImplementedException();
+    }
+}
